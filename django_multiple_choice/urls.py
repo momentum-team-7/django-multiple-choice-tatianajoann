ad4f62955a7d392c3d3d4bf18f976d97421d4aa1
GrantDM <mullen.d.grant@gmail.com>
2021-03-04 12:26:09 -0500

    working django registration

--- django_multiple_choice/urls.py
+++ django_multiple_choice/urls.py
@@ -19,6 +19,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('registration.backends.simple.urls')),
 ]
 
 if settings.DEBUG:
