32884420d09736e8877bb6819b49fadbdb4d164e
GrantDM <mullen.d.grant@gmail.com>
2021-03-04 13:57:23 -0500

    models language and snippet added

--- core/models.py
+++ core/models.py
@@ -3,3 +3,20 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=280)
+
+
+    def __str__(self):
+        return self.name
+
+
+class Snippet(models.Model):
+    type_of = models.CharField(max_length=280)
+    language = models.ForeignKey(Language, on_delete=models.CASCADE, blank=True, null=True, related_name="language")
+    
+
+    def __str__(self):
+        return self.type_of
--- core/views.py
+++ core/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from .models import Language, Snippet
+
 
 # Create your views here.
 
 
 def homepage(request):
     return render(request, 'homepage.html', {})
+
+
