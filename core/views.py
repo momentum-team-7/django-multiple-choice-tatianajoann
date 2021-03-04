0cc0eb368d32a880bcdea7f091b3153e98385fa8
GrantDM <mullen.d.grant@gmail.com>
2021-03-04 14:20:31 -0500

    swapping branches

--- core/views.py
+++ core/views.py
@@ -1,10 +1,11 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from .models import Language, Snippet
 
 
 # Create your views here.
 
-
+@login_required
 def homepage(request):
     return render(request, 'homepage.html', {})
 
