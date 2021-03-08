03224cda3516118a6079ee4576aeff867c7e659b
GrantDM <mullen.d.grant@gmail.com>
2021-03-08 16:05:09 -0500

    working delete button but broken save button

---
+++ static/main.js
@@ -0,0 +1,38 @@
+copyButtons = document.querySelectorAll('.save-button')
+deleteButtons = document.querySelectorAll('.delete-button')
+
+for (let button of copyButtons){
+    button.addEventListener('click', event => {
+        const snippetElement = event.target.parentElement
+        const copyUrl = event.target.dataset.url
+        fetch (copyUrl, {
+            headers: {
+                'Accept': 'application/json',
+                'X-Requested-With': 'XMLHttpRequest',
+            },
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+        })
+    })
+}
+
+
+for (let button of deleteButtons){
+    button.addEventListener('click', event => {
+        const snippetElement = event.target.parentElement
+        const deleteUrl = event.target.dataset.url
+        fetch (deleteUrl, {
+            headers: {
+                'Accept': 'application/json',
+                'X-Requested-With': 'XMLHttpRequest',
+            },
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            snippetElement.remove()
+        })
+    })
+}
